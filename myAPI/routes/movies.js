c658421dd7e3c76e1b80747aec80fe3461307a93
LouisBousiquier <louis.bousiquier@edu.ece.fr>
2019-11-19 18:46:17 +0100

    step2: get avec router et install pm2

--- myAPI/routes/movies.js
+++ myAPI/routes/movies.js
@@ -1,26 +1,40 @@
 const express = require('express');
-//var router = express.Router();
+const router = express.Router();
 
 //const hostname ="localhost";
 //const port = 3000;
 
-let app=express();
+//let app=express();
 let port =8080;
 
 //Creat database
 
-let movies =[{
+let movies = [{
     name: "Forrest Gump",
     id: 0
 }];
 
-app.get('/', function(req,res){
+//GET
+router.get('/', (req,res)=>{
     res.status(200).json({movies})
 })
+
+router.use(port,()=>{
+    console.log('serveur ok')
+})
+
+/*
+/// PUT
+app.put('/',(req,res)=>{
+    res.json({
+movies
+    });
+});
 
 app.listen(port,()=> {
     console.log('le serveur foctionne')
 })
+*/
 /*
 // GET
 router.get('/', (req,res)=>{
@@ -31,14 +45,9 @@ router.get('/', (req,res)=>{
 router.put('/', function(req,res, next){
     res.send('respond with a resource')
 });
+
 */
 
 /*
-/// PUT
-app.put('/',(req,res)=>{
-    res.json({
-        req: req.method,
-        data: "this is a put"
-    });
-});*/
-//module.exports =router;
+*/
+module.exports =router;
